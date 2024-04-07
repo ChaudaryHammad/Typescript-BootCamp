6a96f5865ebb2cce7fdbfbc803db3a9310682a2a
Chaudary Hammad <102407219+ChaudaryHammad@users.noreply.github.com>
2024-04-07 23:27:17 +0500

    raf

--- react-with-ts/src/App.tsx
+++ react-with-ts/src/App.tsx
@@ -1,13 +1,18 @@
 
+import { useState } from 'react'
 import './App.css'
 import Box from './components/Box'
 
 function App() {
+  const [count,setCount] = useState(0)
+  console.log(count);
 
   return (
     <>
    <h1>Typescrip</h1>
-   <Box heading="heading 1" count={50} />
+   <Box heading="heading 1" count={count} func1={()=>{
+  setCount(count+1) 
+   }} />
     </>
   )
 }
--- react-with-ts/src/components/Box.tsx
+++ react-with-ts/src/components/Box.tsx
@@ -2,15 +2,22 @@
 
 type BoxProps = {
     heading:string,
-    count:number
+    count:number,
+    func1:()=>void
 }
 
-const Box = ({heading,count}:BoxProps) => {
+const Box = ({heading,count,func1}:BoxProps) => {
+   
   return (
 <>
 <div>Box</div>
     <h1>{heading}</h1>
     <p>{count}</p>
+    <button onClick={()=>{
+        func1()
+    
+    }}>click me</button>
+    
 </>
   )
 }
