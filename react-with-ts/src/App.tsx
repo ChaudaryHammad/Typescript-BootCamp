9929a6cf8e262e43e09fad11d32fa0b9d873f9ff
Chaudary Hammad <102407219+ChaudaryHammad@users.noreply.github.com>
2024-04-10 23:42:14 +0500

    g

--- react-with-ts/src/App.tsx
+++ react-with-ts/src/App.tsx
@@ -1,26 +1,25 @@
-
-import { useState } from 'react'
-import './App.css'
-import Box from './components/Box'
+import { useState } from "react";
+import "./App.css";
+import Box from "./components/Box";
 
 function App() {
-  const [count,setCount] = useState(0)
+  const [count, setCount] = useState(0);
   console.log(count);
 
   return (
     <>
-   <h1>Typescrip</h1>
-   <Box heading="heading 1" count={count} func1={()=>{
-  setCount(count+1) 
-   }} >
-    {
-
-      
-"hello world"
-    }
-    </Box>
+      <h1>Typescrip</h1>
+      <Box
+        heading="heading 1"
+        count={count}
+        func1={() => {
+          setCount(count + 1);
+        }}
+      >
+        {"hello world"}
+      </Box>
     </>
-  )
+  );
 }
 
-export default App
+export default App;
